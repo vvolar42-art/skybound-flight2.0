2deb00aed9f79c17f62407df932ebe558fd4fd18
vvolar42-art <vvolar42@gmail.com>
2026-08-21 16:29:56 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'flight-sim-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './three.min.js',
+  './icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
